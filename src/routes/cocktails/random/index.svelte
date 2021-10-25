53c93030de25ebe307a9ebdea86d69966b40d7d1
Withered Flowers <withered-flowers@users.noreply.github.com>
2021-10-25 14:41:03 +0700

    feat: fix positioning

--- src/routes/cocktails/random/index.svelte
+++ src/routes/cocktails/random/index.svelte
@@ -27,16 +27,21 @@
 	<button class="btn btn-primary" on:click={onClickHandler}>Randomize Cocktail</button>
 
 	<!-- Card Section -->
-	<div class="grid grid-cols-6 gap-4">
+	<div class="grid grid-cols-6 gap-4 pt-4">
 		<div class="col-start-3 col-span-2">
-			<div class="card text-center shadow-2xl">
+			<div class="card text-center shadow-2xl bg-gray-100">
 				<figure class="px-10 pt-10">
-					<img src={drinkState.thumbnail} alt="Drink Thumbnail" class="rounded-xl" />
+					<img
+						src={drinkState.thumbnail}
+						alt="Drink Thumbnail"
+						class="object-cover object-center rounded-lg shadow-lg h-80"
+					/>
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">{drinkState.name}</h2>
+					<h3 class="py-2 font-semibold">Instructions</h3>
 					<p>{drinkState.instructions}</p>
-					<p>Ingredients</p>
+					<h3 class="py-2 font-semibold">Ingredients</h3>
 					{#each drinkState.ingredients as ingredient}
 						<p>{ingredient.amount} - {ingredient.name}</p>
 					{/each}
