736124287bf51b4e9c50a43f531d137734bee664
Withered Flowers <withered-flowers@users.noreply.github.com>
2021-10-27 07:15:51 +0700

    feat: finalize information

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container min-w-full custom-min-h-screen flex flex-col">
+	<div class="m-auto text-center">
+		<img
+			src="https://avatars.githubusercontent.com/u/1587199?v=4"
+			alt="avatar"
+			class="h-48 w-48 rounded-full mx-auto shadow-lg mb-4"
+		/>
+		<h1>Welcome to Withered Flowers' Project Cocktail</h1>
+		<p>This project is powered by SvelteKit</p>
+		<p>Click the upper right Github Icon to see the source</p>
+	</div>
+</div>
+
+<style>
+	.custom-min-h-screen {
+		min-height: 85vh;
+	}
+</style>
